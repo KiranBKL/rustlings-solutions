a6942b56ae93016f81c5dda7157eb04af976df35
KiranBKL <kirankumarballekallu@gmail.com>
2023-09-29 16:34:40 +0530

    Move_Semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -6,12 +6,41 @@
 // vec0 has length 3 content `[22, 44, 66]`
 // vec1 has length 4 content `[22, 44, 66, 88]`
 
-// I AM NOT DONE
+
+//Here vec0 ownership moved to funtion and function to vec1, we can solve in two ways
+//1.cloning  //vec0 has length 0 content `[]`
+             //vec1 has length 4 content `[22, 44, 66, 88]`
+//2.mutable refernce   //vec0 has length 3 content `[22, 44, 66]`
+                       //vec1 has length 4 content `[22, 44, 66, 88]`
+
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let mut vec1 = fill_vec(vec0.clone());
+
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec1.push(88);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec;
+
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -21,12 +50,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.clone()
 }
