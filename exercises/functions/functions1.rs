7e75ee4ebd85fc96051bb33d776670def130bf52
KiranBKL <kirankumarballekallu@gmail.com>
2023-09-29 12:12:23 +0530

    Functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,13 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// Declare the function
 
 fn main() {
     call_me();
+}
+
+fn call_me()
+{
+    println!("Hey,I am in Milkyway")
 }
