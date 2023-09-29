5a2aa05091915f1d31ecda6600594b084c3bffb6
KiranBKL <kirankumarballekallu@gmail.com>
2023-09-29 21:43:12 +0530

    Strings

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,7 +2,7 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// There r 4 ways
 
 fn main() {
     let answer = current_favorite_color();
@@ -10,5 +10,8 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
+    //"blue".to_string()
+    //"blue".into()
+    //"blue".to_owned()
 }
